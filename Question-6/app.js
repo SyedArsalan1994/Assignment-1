1478157cf4d181f1f9ce7648e34dc1cc636060fb
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-19 16:44:38 +0500

    Create app.js

---
+++ Question-6/app.js
@@ -0,0 +1,9 @@
+"use strict";
+// Store the person's name with space
+let nameWithSpace = "\t  Syed Arsalan Aziz    \t\n";
+// Triming the space from the name
+let trimName = nameWithSpace.trim();
+// Print the name with space
+console.log("Name with Spaces:", nameWithSpace);
+// Print the trimmed name
+console.log("Name without Spaces:", trimName);
