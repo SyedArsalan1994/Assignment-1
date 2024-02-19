3c8a35ca75b90d03ed3c67a68d0242b2bb48f0c9
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-19 16:50:46 +0500

    Program to show user name in uppercase, lowercase and title case

--- Question-3/app.js
+++ Question-3/app.js
@@ -3,4 +3,4 @@
 let personName = "Syed Arsalan Aziz";
 console.log("Hello " + personName.toLowerCase() + " would you like to learn some code today? :)");
 console.log("Hello " + personName.toUpperCase() + " would you like to learn some code today? :)");
-console.log("Hello " + personName.toString() + " would you like to learn some code today? :)");
+console.log("Hello " + personName.toString() + " would you like to learn some code today? :)"); 
