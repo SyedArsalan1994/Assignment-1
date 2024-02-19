1248dc22a9559cc91e6e900e843fa1b4d2376950
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-19 16:03:54 +0500

    Create app.ts

--- Question-3/app.ts
+++ Question-3/app.ts
@@ -1,1 +1,5 @@
-
+//printing persons name with message
+let personName = "Syed Arsalan Aziz";
+console.log("Hello " + personName.toLowerCase() + " would you like to learn some code today? :)");
+console.log("Hello " + personName.toUpperCase() + " would you like to learn some code today? :)");
+console.log("Hello " + personName.toString() + " would you like to learn some code today? :)");
