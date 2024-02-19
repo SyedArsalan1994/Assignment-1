d0c71e20c43edc62b107b31f6bd8bc7638e78424
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-19 16:30:25 +0500

    Create app.js

---
+++ Question-5/app.js
@@ -0,0 +1,5 @@
+"use strict";
+//printing persons name with message
+let famous_person = "Nelson Mandela";
+let message = famous_person + ' once said, "It always seems impossible until its done"';
+console.log(message);
