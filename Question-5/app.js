b33fedbc404904f9694ee7631e11612a6c7e9b3d
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-19 16:52:24 +0500

    Program to write the qoute of famous person with variables concatination

--- Question-5/app.js
+++ Question-5/app.js
@@ -2,4 +2,4 @@
 //printing persons name with message
 let famous_person = "Nelson Mandela";
 let message = famous_person + ' once said, "It always seems impossible until its done"';
-console.log(message);
+console.log(message); 
