3277d182cbcb296ee9db76c0c539008e45679711
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-19 16:50:03 +0500

    Program to print username with sentence through variable

--- Question-2/app.js
+++ Question-2/app.js
@@ -1,4 +1,4 @@
 "use strict";
 //printing persons name with message
 let personName = "Arsalan";
-console.log("Hello " + personName + " would you like to learn some code today? :)");
+console.log("Hello " + personName + " would you like to learn some code today? :)"); 
