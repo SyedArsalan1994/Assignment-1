05839a15052b090bd71cef88668e4a108aa3260e
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 15:33:09 +0500

    Creating the return type function to return the value in string

---
+++ Question-38/app.js
@@ -0,0 +1,8 @@
+"use strict";
+function city_country(City, Country) {
+    return `${City}, ${Country}`;
+}
+// Call the function with city-country pairs and print the returned value
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("New York", "USA"));
+console.log(city_country("Tokyo", "Japan"));
