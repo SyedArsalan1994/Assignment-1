9079aea45a3b5c217910caaf19efe04f66eb07ae
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 16:05:38 +0500

    Created parameterized function to print name of magicians

---
+++ Question-40/app.js
@@ -0,0 +1,9 @@
+"use strict";
+//defininf the magicians function
+function show_magicians(magicians) {
+    magicians.forEach((magician) => {
+        console.log(magician);
+    });
+}
+let magicians = ['Arsalan', 'Bilal', 'Yousuf'];
+show_magicians(magicians);
