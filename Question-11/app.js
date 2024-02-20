b1441b97d926159c7a9ba03c25fad1d1e836adef
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 09:46:46 +0500

    Printing the names in loop with a message

---
+++ Question-11/app.js
@@ -0,0 +1,7 @@
+"use strict";
+//Creating the array of names 
+const names = ["Arsalan", "Yousuf", "Muzaffar", "Ahmed"];
+//creating the loop for array which will print the names
+for (let i = 0; i < names.length; i++) {
+    console.log("Hi " + names[i] + " How are you?");
+}
