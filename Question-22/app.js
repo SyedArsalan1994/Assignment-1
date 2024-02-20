9371822c2a94f72d49256449c20ac188cff78ead
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 12:15:13 +0500

    Conditional checks for true and false

---
+++ Question-22/app.js
@@ -0,0 +1,17 @@
+"use strict";
+let car = "sabaru";
+//looping 10 times to check if 
+for (let i = 0; i <= 5; i++) {
+    if (car == "sabaru")
+        console.log("Is car == 'sabaru'? I predict True.");
+    else {
+        console.log("Is car !== 'sabaru'? I predict False.");
+    }
+}
+for (let i = 0; i <= 5; i++) {
+    if (car == "subaru")
+        console.log("Is car == 'sabaru'? I predict True.");
+    else {
+        console.log("Is car !== 'sabaru'? I predict False.");
+    }
+}
