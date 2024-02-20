0e9c12ef3647e10131d2024c40ad95e2bf9931ad
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 10:10:44 +0500

    Program to send invitations to guests

---
+++ Question-13/app.js
@@ -0,0 +1,8 @@
+"use strict";
+// Store the names of people you'd like to invite to dinner in an array
+const guestList = ["Governor Sindh", "Sir. Zia Khan", "Teachers"];
+// Print a message inviting each person to dinner
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Dear ${guestList[i]}, I would like to invite you to dinner. Please join me for a wonderful evening.`);
+}
+;
