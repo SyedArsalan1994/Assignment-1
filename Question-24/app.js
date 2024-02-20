8b38ee4b6d11cc2173db308090013916d09e399b
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 14:05:59 +0500

    Printing only passing statement

---
+++ Question-24/app.js
@@ -0,0 +1,14 @@
+"use strict";
+let alien_color = "green";
+// Passing version: the alien's color is 'green'
+if (alien_color == "green") {
+    console.log("Congratulations! You just earned 5 points.");
+}
+else {
+}
+// Failing version: the alien's color is 'red'
+if (alien_color == "red") {
+    console.log("Congratulations! You just earned 5 points.");
+}
+else {
+}
