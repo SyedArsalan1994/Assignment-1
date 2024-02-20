848e502db6fd51d0def75c73a3cc9f3ec7457018
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 14:15:30 +0500

    Question 27 Solution

---
+++ Question-26/app.js
@@ -0,0 +1,31 @@
+"use strict";
+let alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (alien_color === 'yellow') {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color === 'red') {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
+let alien_color2 = 'yellow';
+if (alien_color2 === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (alien_color2 === 'yellow') {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color2 === 'red') {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
+let alien_color3 = 'red';
+if (alien_color3 === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else if (alien_color3 === 'yellow') {
+    console.log("Congratulations! You just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color3 === 'red') {
+    console.log("Congratulations! You just earned 15 points for shooting the red alien.");
+}
