e8a21856ee5110833362f8af736317b95c83ad67
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 14:10:05 +0500

    printing statement of fail and pass scenarios with if else conditions

---
+++ Question-25/app.js
@@ -0,0 +1,17 @@
+"use strict";
+let alien_color = 'green';
+// Running the if block: the alien's color is 'green'
+if (alien_color === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else {
+    console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
+let alienColor = 'red';
+// Running the else block: the alien's color is not 'green'
+if (alienColor === 'green') {
+    console.log("Congratulations! You just earned 5 points for shooting the green alien.");
+}
+else {
+    console.log("Congratulations! You just earned 10 points for shooting the alien.");
+}
