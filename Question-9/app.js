6e12320a5c317b8440b3e10f7fde5e965a5ff39b
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 09:34:14 +0500

    Print your favourite number in message variable

---
+++ Question-9/app.js
@@ -0,0 +1,7 @@
+"use strict";
+//This porgram is created to describe your favourite number in message.
+//Initializing the favourite num 
+let favouriteNum = 21;
+let message = 'My favourite number is ' + favouriteNum + '';
+//print message value in console
+console.log(message);
