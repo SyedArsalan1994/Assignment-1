0252b81f8a785d0f40ca7d63801a128014fae5a5
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 11:42:12 +0500

    Printing the list of Landmarks

---
+++ Question-19/app.js
@@ -0,0 +1,19 @@
+"use strict";
+// Create an array of famous landmarks
+const landmarks = [
+    "Eiffel Tower",
+    "Great Wall of China",
+    "Taj Mahal",
+    "Statue of Liberty",
+    "Machu Picchu",
+    "Sydney Opera House",
+    "Colosseum",
+    "Pyramids of Giza",
+    "Christ the Redeemer",
+    "Petra"
+];
+// Print the list of landmarks
+console.log("List of famous landmarks:");
+for (let index = 0; index < landmarks.length; index++) {
+    console.log(`${index}`, landmarks[index]);
+}
