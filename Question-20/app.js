4569bad3971df61ebaad1f4f9ffd75cea5aa6cfa
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 11:49:24 +0500

    Printing object values in console

---
+++ Question-20/app.js
@@ -0,0 +1,12 @@
+"use strict";
+//Creating objects 
+let car = {
+    brand: "Toyota",
+    name: "Corolla",
+    model: 2024,
+    color: "Black"
+};
+console.log(car.name);
+console.log(car.brand);
+console.log(car.color);
+console.log(car.model);
