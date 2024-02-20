3fe35c387087804d6b57625bdf1a86cf8f48fa13
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 15:17:53 +0500

    Created function to print the message on shirt through paramters

---
+++ Question-35/app.js
@@ -0,0 +1,6 @@
+"use strict";
+make_shirts('Medium', 'This shirt is amazing');
+//defining a function of make_shirt
+function make_shirts(size, text) {
+    console.log(`A ${size} sized shirt will be printed with a message "${text}".`);
+}
