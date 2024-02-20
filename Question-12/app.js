4700944fc17556fd9740a7362a5b5796f9a1cdf2
SyedArsalan1994 <59333906+SyedArsalan1994@users.noreply.github.com>
2024-02-20 10:00:47 +0500

    Printing the statements for favorite transportation

---
+++ Question-12/app.js
@@ -0,0 +1,7 @@
+"use strict";
+//Creating the array of Cars
+const Cars = ["Toyota Corolla", "Land Cruiser", "Rolls Royce", "Mercedes"];
+//creating the loop for array which will print the Cars
+for (let i = 0; i < Cars.length; i++) {
+    console.log("I would like to have my own " + Cars[i]);
+}
